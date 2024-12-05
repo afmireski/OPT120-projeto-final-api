f8801d1aeac7c90ef6db7e3c8ccd9fbb7a185eb0
afmireski <afmireski@outlook.com>
2024-12-04 22:38:04 -0300

    #18 - docs: Update openapi

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import authRouter from './auth.router';
+import usersRouter from './users.router';
 
 export const router = Router();
 
-router.use(authRouter);
+router.use(authRouter).use('/api', usersRouter);
 
 export default router;
