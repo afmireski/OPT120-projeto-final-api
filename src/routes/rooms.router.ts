59de5ed49d677408fb76648cd8de58ee6a575d1e
afmireski <afmireski@outlook.com>
2024-12-14 22:11:48 -0300

    #13 - feat: Defined 'api/rooms' route

--- src/routes/rooms.router.ts
+++ src/routes/rooms.router.ts
@@ -1,12 +1,31 @@
 import { Router } from 'express';
-import { updateRoomHandler } from '../controllers/rooms.controller';
+import {
+  listRoomsHandler,
+  updateRoomHandler,
+} from '../controllers/rooms.controller';
 import { authenticationMiddleware } from '../middlewares/authentication.middleware';
 import { internalErrorsMiddleware } from '../middlewares/errors.middleware';
 import { roleMiddleware } from '../middlewares/role.middleware';
 import { validatorMiddleware } from '../middlewares/validators.middleware';
-import { updateRoomSchema } from '../validators/rooms/rooms.schemas';
+import {
+  listRoomsSchema,
+  updateRoomSchema,
+} from '../validators/rooms/rooms.schemas';
+import { extraFieldsMiddleware } from '../middlewares/extraFields.middleware';
+import { paginationMiddleware } from '../middlewares/pagination.middleware';
 
 export const router = Router();
+
+router.get(
+  '/rooms',
+  authenticationMiddleware,
+  roleMiddleware(['ADMIN', 'SERVANT', 'STUDENT']),
+  extraFieldsMiddleware(['filters', 'pagination']),
+  validatorMiddleware(listRoomsSchema, 1),
+  paginationMiddleware,
+  listRoomsHandler,
+  internalErrorsMiddleware,
+);
 
 router.patch(
   '/rooms/:id/update',
