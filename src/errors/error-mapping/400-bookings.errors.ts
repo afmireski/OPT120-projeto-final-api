de335beab18d210633e681919c8c982bb39f25db
afmireski <afmireski@outlook.com>
2025-01-19 18:03:42 -0300

    #8 - feat: Implemenrted 'rejectBookingIntent' logic

--- src/errors/error-mapping/400-bookings.errors.ts
+++ src/errors/error-mapping/400-bookings.errors.ts
@@ -21,6 +21,10 @@ const errors: Record<number, InternalMessage> = {
     message: 'Não foi possível aprovar a reserva',
     httpCode: 400,
   },
+  406: {
+    message: 'Não foi possível rejeitar a reserva',
+    httpCode: 400,
+  },
 };
 
 export default errors;
--- src/services/bookings.service.ts
+++ src/services/bookings.service.ts
@@ -26,3 +26,29 @@ export const approveBookingIntent = async (
     return result;
   });
 };
+
+export const rejectBookingIntent = async (
+  booking_id: number,
+): Promise<Booking> => {
+  const knex = KnexService.getInstance().knex;
+
+  const query = knex('bookings')
+    .update({
+      user_id: null,
+      approved: false,
+      updated_at: knex.fn.now(),
+    })
+    .where('id', booking_id)
+    .where('approved', false)
+    .whereNotNull('user_id')
+    .whereNull('deleted_at')
+    .returning('*');
+
+  return query.then(([result]) => {
+    if (!result) {
+      throw new InternalError(406);
+    }
+
+    return result;
+  });
+}
