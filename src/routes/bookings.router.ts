488a9a12f474f09bf937fa96eff9fcd8ff531016
afmireski <afmireski@outlook.com>
2025-01-19 18:39:11 -0300

    #8 - feat: Implemented '/bookings/:booking_id/cancel' and '/bookings/:booking_id/reject'
    - Implemented routes validators

--- src/routes/bookings.router.ts
+++ src/routes/bookings.router.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import { approveBookingIntentHandler } from '../controllers/bookings.controller';
+import {
+  approveBookingIntentHandler,
+  cancelBookingIntentHandler,
+  rejectBookingIntentHandler,
+} from '../controllers/bookings.controller';
 import { authenticationMiddleware } from '../middlewares/authentication.middleware';
 import { internalErrorsMiddleware } from '../middlewares/errors.middleware';
 import { roleMiddleware } from '../middlewares/role.middleware';
 import { validatorMiddleware } from '../middlewares/validators.middleware';
-import { approveBookingIntentSchema } from '../validators/bookings/bookings.schemas';
+import {
+  approveBookingIntentSchema,
+  cancelBookingIntentSchema,
+  rejectBookingIntentSchema,
+} from '../validators/bookings/bookings.schemas';
 
 export const router = Router();
 
@@ -14,6 +22,24 @@ router.post(
   roleMiddleware(['ADMIN']),
   validatorMiddleware(approveBookingIntentSchema, 1),
   approveBookingIntentHandler,
+  internalErrorsMiddleware,
+);
+
+router.post(
+  '/bookings/:booking_id/reject',
+  authenticationMiddleware,
+  roleMiddleware(['ADMIN']),
+  validatorMiddleware(rejectBookingIntentSchema, 1),
+  rejectBookingIntentHandler,
+  internalErrorsMiddleware,
+);
+
+router.post(
+  '/bookings/:booking_id/cancel',
+  authenticationMiddleware,
+  roleMiddleware(['ADMIN', 'SERVANT', 'STUDENT']),
+  validatorMiddleware(cancelBookingIntentSchema, 1),
+  cancelBookingIntentHandler,
   internalErrorsMiddleware,
 );
 
--- src/validators/bookings/bookings.schemas.ts
+++ src/validators/bookings/bookings.schemas.ts
@@ -13,3 +13,31 @@ export const approveBookingIntentSchema = z.object({
     })
     .strict(),
 });
+
+export const rejectBookingIntentSchema = z.object({
+  params: z
+    .object({
+      booking_id: z
+        .string({
+          message: 'Id inválido',
+        })
+        .regex(/^\d+$/, {
+          message: 'Id deve ser um número válido',
+        }),
+    })
+    .strict(),
+});
+
+export const cancelBookingIntentSchema = z.object({
+  params: z
+    .object({
+      booking_id: z
+        .string({
+          message: 'Id inválido',
+        })
+        .regex(/^\d+$/, {
+          message: 'Id deve ser um número válido',
+        }),
+    })
+    .strict(),
+});
