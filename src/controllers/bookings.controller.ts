ba1252adbc69d7d21190d6110df7f74a78ace308
afmireski <afmireski@outlook.com>
2025-01-19 18:38:14 -0300

    #8 - feat: Implemented rejectBookingIntentHandler and cancelBookingIntentHandler handlers

--- src/controllers/bookings.controller.ts
+++ src/controllers/bookings.controller.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
-import { approveBookingIntent } from '../services/bookings.service';
+import {
+  approveBookingIntent,
+  cancelBookingIntent,
+  rejectBookingIntent,
+} from '../services/bookings.service';
 
 export const approveBookingIntentHandler = async (
   req: Request,
@@ -13,6 +17,45 @@ export const approveBookingIntentHandler = async (
   return approveBookingIntent(Number(booking_id))
     .then((booking) => {
       res.status(200).json(booking);
+    })
+    .catch((e) => {
+      next(e);
+    });
+};
+
+export const rejectBookingIntentHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const {
+    params: { booking_id },
+  } = req;
+
+  return rejectBookingIntent(Number(booking_id))
+    .then((booking) => {
+      res.status(200).json(booking);
+    })
+    .catch((e) => {
+      next(e);
+    });
+};
+
+export const cancelBookingIntentHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const {
+    params: { booking_id },
+    user,
+  } = req;
+
+  const userId = user!.id;
+
+  return cancelBookingIntent(Number(booking_id), userId)
+    .then(() => {
+      res.status(200).json();
     })
     .catch((e) => {
       next(e);
